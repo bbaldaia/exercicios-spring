4d2117ec5dba51ea0d82ef923a1d0be80ce34eab
bbaldaia <brunobfreire98@gmail.com>
2021-08-01 16:25:32 -0300

    Exercícios Spring

---
+++ ex.30.07/lojagames/src/main/java/br/org/generation/lojagames/LojagamesApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.lojagames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class lojagamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(lojagamesApplication.class, args);
+	}
+
+}
